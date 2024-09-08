e3c50d7c8570a89381c947e58e01fa5c6f3e9656
devcarolinealmeida <carolineitalia85@gmail.com>
2024-09-08 17:58:25 +0200

    UPDATE: add class utilities section-title e section-description

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -11,10 +11,10 @@ export const ProductShowcase = () => {
           <div className="flex justify-center">
             <div className="tag">Boost your productivity</div>
           </div>
-          <h2 className="text-center text-3xl md:text-[3.375rem] md:leading-[3.75rem] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+          <h2 className="section-title mt-5">
             A more effective way to track progress
           </h2>
-          <p className="text-center text-[1.37rem] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+          <p className="section-description mt-5">
             Effortlessly turn your ideas into a fully functional, responsive,
             no-code SaaS website in just minutes with the set of free components
             for Framer.
