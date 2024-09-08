ab7d3e4278764f5cdcb354ff0d54a239a543a0ef
devcarolinealmeida <carolineitalia85@gmail.com>
2024-09-08 17:44:52 +0200

    ADD: Product Showcase

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,42 @@
+import productImage from "@/assets/product-image.png";
+import piramidImage from "@/assets/pyramid.png";
+import tubeImage from '@/assets/tube.png';
+import Image from "next/image";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-grafient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="text-center text-3xl md:text-[3.375rem] md:leading-[3.75rem] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="text-center text-[1.37rem] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            Effortlessly turn your ideas into a fully functional, responsive,
+            no-code SaaS website in just minutes with the set of free components
+            for Framer.
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={productImage} className="mt-10" alt="Product Image" />
+          <Image
+            src={piramidImage}
+            height={262}
+            width={262}
+            className="absolute hidden md:block -right-36 -top-32"
+            alt="Piramid Image"
+          />
+          <Image
+            src={tubeImage}
+            height={248}
+            className="absolute hidden md:block bottom-24 -left-36"
+            alt="Tube Image"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
