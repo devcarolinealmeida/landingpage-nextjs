510bc4767977456821bda2db58a9bc6723fa9073
devcarolinealmeida <carolineitalia85@gmail.com>
2024-09-08 17:15:45 +0200

    ADD: LogoTicker

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,26 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image className="logo-ticker-image" src={acmeLogo} alt="acme Logo"/>
+            <Image className="logo-ticker-image" src={quantumLogo} alt="quantum Logo"/>
+            <Image className="logo-ticker-image" src={echoLogo} alt="echo Logo"/>
+            <Image className="logo-ticker-image" src={celestialLogo} alt="celestial Logo"/>
+            <Image className="logo-ticker-image" src={pulseLogo} alt="pulse Logo"/>
+            <Image className="logo-ticker-image" src={apexLogo} alt="apex Logo"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
